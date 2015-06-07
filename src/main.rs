b37c4fcd16f7f99d86f4bb2f25cb5cd9327bcab3
Joel McCracken <mccracken.joel@gmail.com>
2015-06-07 18:49:49 -0400

    nicely formatted times being logged.

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use std::fs::File;
 use std::io;
 use chrono::*;
 
-
-fn log_something(filename: &'static str, string: &'static [u8; 12]) -> io::Result<()> {
+fn log_time(filename: &'static str) -> io::Result<()> {
+    let local: DateTime<Local> = Local::now();
+    let formatted = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
+    let bytes = formatted.as_bytes();
     let mut f = try!(File::create(filename));
-    try!(f.write_all(string));
+    try!(f.write_all(bytes));
     Ok(())
 }
 
 fn main() {
-    let local: DateTime<Local> = Local::now();
-    println!("{}", local);
-    match log_something("log.txt", b"ITS ALIVE!!!") {
+    match log_time("log.txt") {
         Ok(..) => println!("File created!"),
         Err(..) => println!("Error: could not create file.")
     }

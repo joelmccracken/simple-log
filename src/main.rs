fccaf3b1b4d40421ebac41faf93177ceb74eebce
Joel McCracken <mccracken.joel@gmail.com>
2015-07-12 01:07:32 -0400

    finished clap example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 extern crate clap;
-use clap::App;
+use clap::{App,Arg};
 
 fn main() {
-  let _ = App::new("fake").version("v1.0-beta").get_matches();
+    let matches = App::new("simple-log").version("v0.0.1")
+        .arg(Arg::with_name("LOG FILE")
+             .short("l")
+             .long("logfile")
+             .required(true)
+             .takes_value(true))
+        .arg(Arg::with_name("AUTH TOKEN")
+             .short("t")
+             .long("token")
+             .takes_value(true))
+        .get_matches();
+
+    let logfile_path = matches.value_of("LOG FILE").unwrap();
+    let auth_token   = matches.value_of("AUTH TOKEN");
 }
+
